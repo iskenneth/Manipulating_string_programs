42d22111d3aae9bf8b860ada3b3ce56534f2efbe
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 18:35:39 +0800

    input

--- without_swapcase.py
+++ without_swapcase.py
@@ -1,4 +1,5 @@
 #Input: Get a string from the user.
+string = input("Enter a string: ")
 #Loop: Go through each character in the string.
 #If the character is in uppercase (A-Z), replace it with its corresponding lowercase (a-z).
 #If the character is in lowercase (a-z), replace it with its corresponding uppercase (A-Z).
