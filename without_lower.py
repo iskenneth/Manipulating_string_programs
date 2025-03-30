5a11932cf0a7c5373e602c2d18f630ee5e62fac5
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 15:11:09 +0800

    converting upper to lower using if else

--- without_lower.py
+++ without_lower.py
@@ -1,7 +1,14 @@
 # Get input string
 string = input("Enter a string: ")
 # Initialize empty string to store lowercase result
+lowercase = ""
 # Loop through each character in the string
-# Check if the character is an uppercase letter ('A' to 'Z')
-# Convert to lowercase by using ASCII character mapping
+for char in string:
+    # Check if the character is an uppercase letter ('A' to 'Z')
+    if 'A'  <= char <= 'Z' :
+        # Convert to lowercase by using ASCII character mapping
+        lowercase += chr(char + ('a' - 'A'))
+    else:
+        lowercase += char 
+    
 # Print the result
