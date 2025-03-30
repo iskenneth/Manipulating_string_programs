ce35e055cbd0fbb6fc7bb613cdddc29e5690e932
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 18:22:39 +0800

    input

--- without_center.py
+++ without_center.py
@@ -1,4 +1,6 @@
 #Get input string and desired length.
+string = input("Enter a string: ")
+margin = int(input("Enter desire margin: "))
 #Calculate spaces needed on both sides.
 #Add spaces before and after the string.
 #Print the modified string.
