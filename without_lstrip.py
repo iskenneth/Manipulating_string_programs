f0c80969927ac81765a735a0fbf34e8ada3be654
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 13:49:05 +0800

    pseudocode

---
+++ without_lstrip.py
@@ -0,0 +1,5 @@
+#Get input string.
+#Loop through characters from the beginning:
+#If the character is a space, move to the next character.
+#If the character is not a space, stop looping.
+#Print the remaining string.
