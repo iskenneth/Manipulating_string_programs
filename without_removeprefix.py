e3848a04507237d954c8daaee443fc93689ae9e5
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 14:56:40 +0800

    checking string and removing prefix

--- without_removeprefix.py
+++ without_removeprefix.py
@@ -1,6 +1,7 @@
 #Get input string and prefix to remove.
 string = input("Enter a string: ")
 prefix = input("Enter prefix to be remove: ")
-#Check if the string starts with the prefix:
-#If yes, remove the prefix.
+#Check if the string starts with the prefix
+if string [ : len(prefix)] == prefix: #If yes, remove the prefix.
+    string = string[len(prefix) : ]
 #If no, return the string unchanged.
