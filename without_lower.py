f508e4db2af999a179d5b666ba7486342de206fe
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 15:04:58 +0800

    pseudocode

---
+++ without_lower.py
@@ -0,0 +1,6 @@
+# Get input string
+# Initialize empty string to store lowercase result
+# Loop through each character in the string
+# Check if the character is an uppercase letter ('A' to 'Z')
+# Convert to lowercase by using ASCII character mapping
+# Print the result
