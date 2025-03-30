acfa900c1a99555d128cf5a0d9af06d642bb4182
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 18:34:52 +0800

    Pseudocode

---
+++ without_swapcase.py
@@ -0,0 +1,5 @@
+#Input: Get a string from the user.
+#Loop: Go through each character in the string.
+#If the character is in uppercase (A-Z), replace it with its corresponding lowercase (a-z).
+#If the character is in lowercase (a-z), replace it with its corresponding uppercase (A-Z).
+#Output: Print the modified string.
