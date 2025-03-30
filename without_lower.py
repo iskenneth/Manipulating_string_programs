9c7a40992b884ff0236a74be9cf0d50b14c3f77a
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 15:07:04 +0800

    input

--- without_lower.py
+++ without_lower.py
@@ -1,4 +1,5 @@
 # Get input string
+string = input("Enter a string: ")
 # Initialize empty string to store lowercase result
 # Loop through each character in the string
 # Check if the character is an uppercase letter ('A' to 'Z')
