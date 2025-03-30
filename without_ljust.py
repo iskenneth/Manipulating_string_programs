b5bb40e88ff12fe826a9d23ec5a44af2200d17d5
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 16:42:10 +0800

    calculation of justify size

--- without_ljust.py
+++ without_ljust.py
@@ -2,5 +2,7 @@
 string = ("Enter your string: ")
 justify = int(input("Enter desired justify length: "))
 #Calculate how many spaces are needed to reach the length.
+if len(string) < justify:
+    s += " " * (length - len(string))
 #Append the required spaces at the end of the string.
 #Print the modified string.
