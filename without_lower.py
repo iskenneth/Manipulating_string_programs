fd6f1d267dfbb66473b2598ba70527af3ff63f0e
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 15:20:03 +0800

    Remove if else statement and add an uppercase lowercase letter to verify the string

--- without_lower.py
+++ without_lower.py
@@ -2,13 +2,17 @@
 string = input("Enter a string: ")
 # Initialize empty string to store lowercase result
 lowercase = ""
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
 # Loop through each character in the string
 for char in string:
     # Check if the character is an uppercase letter ('A' to 'Z')
-    if 'A'  <= char <= 'Z' :
-        # Convert to lowercase by using ASCII character mapping
-        lowercase += chr(char + ('a' - 'A'))
+    if char  in uppercase:
+        # Find the index of the uppercase letter in uppercase string
+        index = uppercase.index(char)
+        # Get the corresponding lowercase letter
+        lowercase += lowercase_letters[index]
     else:
-        lowercase += char 
-    
+        lowercase += char  
 # Print the result
+print ("Output: ", lowercase)
