3002b7e5537b9cfecddb332c6960cf426f1c50fa
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 18:39:19 +0800

    defining upper and lowercase letter

--- without_swapcase.py
+++ without_swapcase.py
@@ -1,5 +1,7 @@
 #Input: Get a string from the user.
 string = input("Enter a string: ")
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
 #Loop: Go through each character in the string.
 #If the character is in uppercase (A-Z), replace it with its corresponding lowercase (a-z).
 #If the character is in lowercase (a-z), replace it with its corresponding uppercase (A-Z).
