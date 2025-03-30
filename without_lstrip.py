77e99685849f21fce140c521c25668b5c5791523
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 13:55:54 +0800

    loop through strings

--- without_lstrip.py
+++ without_lstrip.py
@@ -1,6 +1,9 @@
 #Get input string.
-s = input("Enter a string with a leading spaces: ")
+string = input("Enter a string with a leading spaces: ")
 #Loop through characters from the beginning:
+i = 0
+while i < 0 len(string) and string[i] == " ":
+    i += 1
 #If the character is a space, move to the next character.
 #If the character is not a space, stop looping.
 #Print the remaining string.
