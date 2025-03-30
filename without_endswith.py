53d58fd096bb222caa526104d222068d48618370
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 16:14:08 +0800

    checking of suffix

--- without_endswith.py
+++ without_endswith.py
@@ -1,5 +1,6 @@
 #Get input string and suffix to check.
 string = input ("Enter a string: ")
 ending = input ("Enter suffix to detect: ")
-#Compare the last characters of the string with the suffix.
+#Compare the last characters of the string with the suffix
+if len(ending) <= len(string) and string[- len(ending) : ] == ending:
 #If they match, print True, else print False.
