a41c853f65c46bd162acb98df7c3f51c2b7db0fb
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-30 14:52:15 +0800

    Input

--- without_removeprefix.py
+++ without_removeprefix.py
@@ -1,4 +1,6 @@
 #Get input string and prefix to remove.
+string = input("Enter a string: ")
+prefix = input("Enter prefix to be remove: ")
 #Check if the string starts with the prefix:
 #If yes, remove the prefix.
 #If no, return the string unchanged.
