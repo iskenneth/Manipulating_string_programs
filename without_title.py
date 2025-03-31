b1ed1f0db1b1281ea49a9d4406a767aaf5bd4472
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-31 23:01:30 +0800

    pseudocode

---
+++ without_title.py
@@ -0,0 +1,7 @@
+#Get the input string.
+
+#Split the string into words.
+
+#Convert the first letter of each word to uppercase and the rest to lowercase.
+
+#Join the words back together and print the result.
