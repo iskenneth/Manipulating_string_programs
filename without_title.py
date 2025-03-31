77d1812a09da7c0f598caab330547268beebf531
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-31 23:05:47 +0800

    looping each word and capitalizing first letter

--- without_title.py
+++ without_title.py
@@ -3,5 +3,7 @@ string = input("Enter a string: ")
 #Split the string into words.
 words = string.split()
 #Convert the first letter of each word to uppercase and the rest to lowercase.
+for word in words:
+    title = word[ 0 ].upper() + word[ 1: ].lower()
 
 #Join the words back together and print the result.
