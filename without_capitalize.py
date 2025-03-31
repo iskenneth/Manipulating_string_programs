8c07801e6938a1f50cfb85c970a10269301134c9
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-31 22:37:44 +0800

    if else statement for capitalization

--- without_capitalize.py
+++ without_capitalize.py
@@ -1,6 +1,10 @@
 #Get the input string.
 string = input("Enter a string: ")
 #If the string is not empty:
+if string:
+    capitalized = string[0].upper() + string [ 1: ].lower()
+else:
+    capitalized = "
 #Convert the first character to uppercase.
 #Convert the rest of the string to lowercase.
 #Print the modified string.
