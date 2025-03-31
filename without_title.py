820d1b6357fd570de51a950c02bc83d540259c64
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-31 23:08:44 +0800

    putting the new words in storage

--- without_title.py
+++ without_title.py
@@ -6,4 +6,5 @@ title_storage = []
 #Convert the first letter of each word to uppercase and the rest to lowercase.
 for word in words:
     title = word[ 0 ].upper() + word[ 1: ].lower()
+    title_storage.append(title)
 #Join the words back together and print the result.
