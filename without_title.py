20a89fa683d471259e8c881205263c5a632af825
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-31 23:07:48 +0800

    added storage for new wprds

--- without_title.py
+++ without_title.py
@@ -2,8 +2,8 @@
 string = input("Enter a string: ")
 #Split the string into words.
 words = string.split()
+title_storage = [] 
 #Convert the first letter of each word to uppercase and the rest to lowercase.
 for word in words:
     title = word[ 0 ].upper() + word[ 1: ].lower()
-
 #Join the words back together and print the result.
