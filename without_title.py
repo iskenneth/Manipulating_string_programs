0461320181d71eca55737a9c0fc845c336497594
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-03-31 23:03:13 +0800

    spliting words in string

--- without_title.py
+++ without_title.py
@@ -1,7 +1,7 @@
 #Get the input string.
 string = input("Enter a string: ")
 #Split the string into words.
-
+words = string.split()
 #Convert the first letter of each word to uppercase and the rest to lowercase.
 
 #Join the words back together and print the result.
