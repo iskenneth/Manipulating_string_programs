779908af0e6ae40a8672a2beb58ae8425f86fbca
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-04 22:33:52 +0800

    finding and removing suffix in the string using if else statement

--- without_removesuffix.py
+++ without_removesuffix.py
@@ -3,4 +3,8 @@ string = input("Enter a string: ")
 #Ask for suffix
 suffix = input("What is your suffix?")
 #If text ends with suffix, remove it
+if string[-len(suffix) : ] == suffix:
+    print(string[: -len(suffix)]) 
+else: 
+    print(string)
 #Print result
