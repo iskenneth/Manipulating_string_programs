6096da552c48c29ccd021d9021ccdd582d050886
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-04 22:42:37 +0800

    defining values of capital and lower case letter

--- without_upper.py
+++ without_upper.py
@@ -1,6 +1,8 @@
 #Ask the user to enter text  
 text = input("Enter your string")
 #define the value of lower and capital letter
-#create a result storahe
+capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "abcdefghijklmnopqrstuvwxyz"
+#create a result storage
 #For each letter in the text:  ,If the letter is in small letters:    Find its position in small letters    Add the letter from big letters at the same position to result  
 #Show the result
