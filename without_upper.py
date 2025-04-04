30a9846757a4f5bd4ad268d8a02515ce7341517d
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-04 22:39:10 +0800

    pseudocode

---
+++ without_upper.py
@@ -0,0 +1,5 @@
+#Ask the user to enter text  
+#define the value of lower and capital letter
+#create a result storahe
+#For each letter in the text:  ,If the letter is in small letters:    Find its position in small letters    Add the letter from big letters at the same position to result  
+#Show the result
