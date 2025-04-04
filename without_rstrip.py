bb8fbd85b95c01d840845a1855c20d2f7d4a2e40
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-04 22:24:10 +0800

    finding and removing spaces

--- without_rstrip.py
+++ without_rstrip.py
@@ -1,5 +1,8 @@
 #Get text
 text = input("Enter your string: ")
 #Start from last character
+check = len(text) - 1
 #While character is space, move left
+while check >= 0 and text[check] == ' ':
+    check -= 1
 #Print characters up to that point
