8249233d06835e561573fca0ad6c59b18c76124f
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-04 22:48:01 +0800

    finding lowercase letter and changing it to capital

--- without_upper.py
+++ without_upper.py
@@ -1,8 +1,15 @@
 #Ask the user to enter text  
-text = input("Enter your string")
+text = input("Enter your string: ")
 #define the value of lower and capital letter
 capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
 #create a result storage
-#For each letter in the text:  ,If the letter is in small letters:    Find its position in small letters    Add the letter from big letters at the same position to result  
+output = ""
+#For each letter in the text:  ,If the letter is in small letters:    Find its position in small letters    Add the letter from big letters at the same position to result 
+for letter in text:
+    if letter in text in lower:
+        index = lower.index(letter) 
+        output += capital(index)
+else:
+    output += letter
 #Show the result
