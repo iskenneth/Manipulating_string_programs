a05a5bc578d452ec9ada71c54c9b31df74d34ff7
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-06 10:08:24 +0800

    Assuming string is in lower case

--- without_islower.py
+++ without_islower.py
@@ -1,5 +1,6 @@
 #Ask user for text
 Input = input("Enter a string: ")
 #Assume all letters are lowercase
+islower = True 
 #For each letter: If it's an uppercase letter, set result to false
 #Show result
