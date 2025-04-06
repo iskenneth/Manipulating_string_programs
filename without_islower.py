e9c05e40f2d65597855e00fe8a08b15683b71329
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-04-06 10:09:24 +0800

    assigning values to lower case and uppercase

--- without_islower.py
+++ without_islower.py
@@ -1,6 +1,8 @@
 #Ask user for text
 Input = input("Enter a string: ")
 #Assume all letters are lowercase
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 islower = True 
 #For each letter: If it's an uppercase letter, set result to false
 #Show result
